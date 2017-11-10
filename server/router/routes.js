7cb982f4401b151dfcc04ae2474dea0abbfe52c9
Brendan Viloria <bjviloria104@gmail.com>
2017-11-10 12:18:59 -0800

    [ops] -Add controller and routes

---
+++ server/router/routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const controller = require('../controller/controller.js');
+
+router.route('/food')
+  .post(controller.food);
+
+router.route('/active')
+  .post(controller.active);
+
+router.route('/movies')
+  .post(controller.movies);
+
+router.route('/entertainment')
+  .post(controller.entertainment);
+
+router.route('/random')
+  .post(controller.random);
