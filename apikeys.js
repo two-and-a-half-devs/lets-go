1784cdd216bc740f127993dba2c4d2cbbdeaa45f
Brendan Viloria <bjviloria104@gmail.com>
2018-02-02 16:48:24 -0800

    fix more for heroku

---
+++ apikeys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  yelp: ' bearer vdJ8SzNZaeS5Gye-wdyUNZYYprt_Wb_-14rocHG9wBFftihBl3OXekPnShZakKmoFKj5HrfK6UMehpUXOBziyy97AmRbH4r2CBwV053WuCLVwgRWEnCWXDKIfnoCWnYx',
+  movie: 'Token R8KT7m2U0ajrXQPmPjEzMELe00ySL8qM',
+  event: 'Bearer 7CR565GXS22IJZJ46NMS'
+}
